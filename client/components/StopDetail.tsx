95bc2cb3c82ece4cd01647db3658e4ef62287730
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-14 13:06:05 +1200

    List stop departure origins

--- client/components/StopDetail.tsx
+++ client/components/StopDetail.tsx
@@ -1,5 +1,23 @@
+import { useQuery } from '@tanstack/react-query'
+import { getStopPredictions } from '@/api/stopInfo'
+
+const stopId = 'WATE'
+
 function StopDetail() {
-  return <></>
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['stop', stopId],
+    queryFn: () => getStopPredictions(stopId),
+  })
+
+  const stopDepartures = data?.departures
+
+  return (
+    <>
+      {stopDepartures?.map((departure, index) => {
+        ;<p key={index}>{departure.origin.name}</p>
+      })}
+    </>
+  )
 }
 
 export default StopDetail
