35d7ca00f34660ecb1a6a2366f97e071fd8f6b19
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-14 12:46:54 +1200

    Send stop id value to the server to get prediction

--- client/api/stopInfo.ts
+++ client/api/stopInfo.ts
@@ -2,6 +2,8 @@ import request from 'superagent'
 import { Welcome, Departure } from '@/models/stopPredictions'
 
 export async function getStopPredictions(stopId: string): Promise<Welcome> {
-  const response = await request.get('api/v1/metlink')
+  const response = await request.get(
+    `api/v1/metlink/prediction?stop-id=${stopId}`,
+  )
   return response.body
 }
