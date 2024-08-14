2e51f3efed67036200c99f95cb9e787494de89c4
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-14 16:45:54 +1200

    Display data on table ui

--- client/components/StopDetail.tsx
+++ client/components/StopDetail.tsx
@@ -1,6 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
 import { getStopPredictions } from '@/api/stopInfo'
 import { Departure } from '@/models/stopPredictions'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 const stopId = 'WATE'
 
@@ -20,10 +29,42 @@ function StopDetail() {
   return (
     <>
       <div>
-        <h2>Predictions</h2>
-        {stopDepartures.map((departure, index) => {
-          return <p key={index}>to {departure.destination.name}</p>
-        })}
+        <h2>Departures for {stopId}</h2>
+        <Table>
+          <TableCaption>Up-coming departures</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Service</TableHead>
+              <TableHead>Destination</TableHead>
+              <TableHead>Direction</TableHead>
+              <TableHead>Scheduled</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Delay</TableHead>
+              <TableHead className="text-right">Wheelchair Access</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {stopDepartures.map((departure, index) => {
+              return (
+                <>
+                  <TableRow key={index}>
+                    <TableCell>{departure.service_id}</TableCell>
+                    <TableCell className="font-medium">
+                      {departure.destination.name}
+                    </TableCell>{' '}
+                    <TableCell>{departure.direction}</TableCell>
+                    <TableCell>{departure.arrival.aimed as string}</TableCell>
+                    <TableCell>{departure.status}</TableCell>
+                    <TableCell>{departure.delay}</TableCell>
+                    <TableCell className="text-right">
+                      {departure.wheelchair_accessible}
+                    </TableCell>
+                  </TableRow>
+                </>
+              )
+            })}
+          </TableBody>
+        </Table>
       </div>
     </>
   )
