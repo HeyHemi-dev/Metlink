5ed3ecc8b3435336add91533ff7d5b3a896bc5d3
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-08-19 08:50:33 +1200

    Make a start; render stops for each route  via API

--- client/api/stopInfo.ts
+++ client/api/stopInfo.ts
@@ -1,11 +1,20 @@
 import request from 'superagent'
-import { Welcome } from 'models/metlink'
+import { Stop, Welcome } from 'models/metlink'
 
 export async function getStopPredictions(stopId: string): Promise<Welcome> {
   const requestUrl = `/api/v1/metlink/predictions?stop_id=${stopId}`
-  console.log('Request to', requestUrl)
+  //console.log('Request to', requestUrl)
   const response = await request.get(requestUrl)
-  console.log('Response', response.body)
+  //console.log('Response', response.body)
+
+  return response.body
+}
+
+export async function getStopsForRoute(routeId: string): Promise<Stop[]> {
+  const requestUrl = `/api/v1/metlink/stops?route_id=${routeId}`
+  //console.log('Request to', requestUrl)
+  const response = await request.get(requestUrl)
+  //console.log('Response', response.body)
 
   return response.body
 }
--- client/components/Stops.tsx
+++ client/components/Stops.tsx
@@ -1,66 +1,59 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { RouteStops } from 'models/metlink'
 import { metlinkRoutes } from '../../data/metlinkRoutes'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+import { getStopsForRoute } from '@/api/stopInfo'
 import { useQuery } from '@tanstack/react-query'
 
 function Stops() {
-  // const query = useQuery({
-  //   queryKey: [],
-  //   queryFn: () =>
-  // })
-
-  const railRoutes = metlinkRoutes.filter(
-    (route) => route.agency_id.toLowerCase() === 'Rail'.toLowerCase(),
-  )
-  const railRouteStops = railRoutes
-  railRouteStops.map((route) => {
-    route.stops = [
-      {
-        stop_id: 'string',
-        stop_code: 'string',
-        stop_name: 'string',
-        stop_desc: 'string',
-        stop_lat: 0,
-        stop_lon: 0,
-        zone_id: 'string',
-        stop_url: 'string',
-        location_type: 0,
-        parent_station: 'string',
-        stop_timezone: 'string',
-      },
-    ]
+  const query = useQuery({
+    queryKey: [],
+    queryFn: () => getStopsForRoute('5'),
   })
-  console.log(railRouteStops)
 
-  return (
-    <>
-      <h2>Stops</h2>
-      <Tabs defaultValue={railRoutes[0].route_short_name} className="w-[400px]">
-        <TabsList>
-          {railRoutes.map((route, index) => {
+  if (query.data) {
+    const railRoutes = metlinkRoutes.filter(
+      (route) => route.agency_id.toLowerCase() === 'Rail'.toLowerCase(),
+    )
+    const railRouteStops: RouteStops[] = railRoutes.map((route) => ({
+      ...route,
+      stops: query.data,
+    }))
+
+    console.log('railRouteStops', railRouteStops)
+
+    return (
+      <>
+        <h2>Stops</h2>
+        <Tabs
+          defaultValue={railRouteStops[0].route_short_name}
+          className="w-[400px]"
+        >
+          <TabsList>
+            {railRouteStops.map((route, index) => {
+              return (
+                <TabsTrigger value={route.route_short_name} key={index}>
+                  {route.route_short_name}
+                </TabsTrigger>
+              )
+            })}
+          </TabsList>
+          {railRouteStops.map((route, index) => {
             return (
-              <TabsTrigger value={route.route_short_name} key={index}>
-                {route.route_short_name}
-              </TabsTrigger>
+              <TabsContent value={route.route_short_name} key={index}>
+                <h2>{`Stops - ${route.route_long_name}`}</h2>
+                {route.stops.map((stop, index) => {
+                  return <p key={index}>{stop.stop_name}</p>
+                })}
+              </TabsContent>
             )
           })}
-        </TabsList>
-        {railRoutes.map((route, index) => {
-          return (
-            <TabsContent value={route.route_short_name} key={index}>
-              {route.stops.map((stop, index) => {
-                return <p key={index}>{stop.stop_name}</p>
-              })}
-            </TabsContent>
-          )
-        })}
-        <TabsContent value="account">
-          Make changes to your account here.
-        </TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
-      </Tabs>
-    </>
-  )
+        </Tabs>
+      </>
+    )
+  }
+
+  return <div>Loading...</div>
 }
 
 export default Stops
